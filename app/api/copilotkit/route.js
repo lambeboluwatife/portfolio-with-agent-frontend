7e52e12493c1c7ed9808470c1323dee35f781561
Lambe Boluwatife <boluwatifelambe@gmail.com>
2025-08-08 19:19:49 +0100

    copilotkit route.js

--- app/api/copilotkit/route.js
+++ app/api/copilotkit/route.js
@@ -4,26 +4,25 @@ import {
   copilotRuntimeNextJSAppRouterEndpoint,
 } from "@copilotkit/runtime";
 
-import { MastraAgent } from "@ag-ui/mastra"
+import { MastraAgent } from "@ag-ui/mastra";
 import { MastraClient } from "@mastra/client-js";
- 
+
 const MASTRA_URL = process.env.MASTRA_URL || "http://localhost:4111";
- 
+
 const serviceAdapter = new ExperimentalEmptyAdapter();
- 
 
 const runtime = new CopilotRuntime({
   agents: await MastraAgent.getRemoteAgents({
     mastraClient: new MastraClient({ baseUrl: MASTRA_URL }),
   }),
 });
- 
+
 export const POST = async (req) => {
   const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
-    runtime, 
+    runtime,
     serviceAdapter,
     endpoint: "/api/copilotkit",
   });
- 
+
   return handleRequest(req);
 };
